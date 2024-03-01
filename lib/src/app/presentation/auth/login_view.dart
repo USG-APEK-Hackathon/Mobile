6ac20fc72c403b920c77f9c2a0380949756889fa
İlham Süleymanov <ilhamsuleymanov40@gmail.com>
2024-03-01 14:31:28 +0400

    Add health and permission dependencies, and implement health authorization in LoginView

--- lib/src/app/presentation/auth/login_view.dart
+++ lib/src/app/presentation/auth/login_view.dart
@@ -1,10 +1,38 @@
 import 'package:auto_route/annotations.dart';
 import 'package:flutter/material.dart';
+import 'package:health/health.dart';
 
 /// The view for the login page
 @RoutePage()
-class LoginView extends StatelessWidget {
+class LoginView extends StatefulWidget {
   const LoginView({super.key});
+
+  @override
+  State<LoginView> createState() => _LoginViewState();
+}
+
+class _LoginViewState extends State<LoginView> {
+  @override
+  void initState() {
+    super.initState();
+    _requestPermissions();
+  }
+
+  Future<void> _requestPermissions() async {
+    final HealthFactory health = HealthFactory();
+    bool isAuthorized = await health.requestAuthorization(
+      [
+        HealthDataType.STEPS,
+      ],
+    );
+
+    if (isAuthorized) {
+      print('Authorized');
+    } else {
+      print('Not authorized');
+    }
+  }
+  // Request permissions
 
   @override
   Widget build(BuildContext context) {
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -5,6 +5,7 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
+import 'package:apex_mobile/src/app/presentation/auth/login_view.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
@@ -13,7 +14,7 @@ import 'package:apex_mobile/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const LoginView());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
