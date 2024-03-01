84813f044a86dc134565e6b86c8c4713338f26b2
İlham Süleymanov <ilhamsuleymanov40@gmail.com>
2024-03-01 11:16:18 +0400

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:apex_mobile/src/app.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  // Ensures that the Flutter binding is initialized before the app starts
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // Run the app
+  runApp(const App());
+}
